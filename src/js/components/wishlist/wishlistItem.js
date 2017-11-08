09e9f98f3c6c4b3b9c465b47a5d7c1774c2d50cf
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-11-08 17:06:00 +0800

    add wishlist item image

--- src/js/components/wishlist/wishlistItem.js
+++ src/js/components/wishlist/wishlistItem.js
@@ -10,13 +10,17 @@ const WishlistItem = ({ item }) => {
 
   return (
     <div className="item_box">
-      <div className="item_title">
-        {title} <span className="item_year">({year})</span>
+      <div className="item_image">
+        <img alt={title} src="https://images-na.ssl-images-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg" />
       </div>
-      <hr className={titleHrStyle} />
-      <div>IMDB<Grade classes={{ root: 'rating_star' }} color="yellow" /> {rating}</div>
-      <div>
-        {tags.map((tag) => <a className="item_tag" href="javascript.void(0)" key={tag}>{tag}</a>)}
+      <div className="item_details">
+        <div className="item_title">{title}</div>
+        <div className="item_year">({year})</div>
+        <hr className={titleHrStyle} />
+        <div>IMDB<Grade classes={{ root: 'rating_star' }} color="#fff000" /> {rating}</div>
+        <div>
+          {tags.map((tag) => <a className="item_tag" href="javascript.void(0)" key={tag}>{tag}</a>)}
+        </div>
       </div>
     </div>
   );
